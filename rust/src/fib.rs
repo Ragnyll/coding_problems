f0f1a8af16c4a17146ac35aa547e62ccc26c436e
ragnyll <ragnyll@gallowzhumour.dev>
2021-09-03 18:08:59 -0500

    more memoization variants

--- rust/src/fib.rs
+++ rust/src/fib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[allow(dead_code)]
 fn fib_recursive(n: usize) -> usize {
     if n < 3 {
@@ -25,9 +27,51 @@ fn fib(number: usize) -> usize {
     fib_memo(number, &mut vec![None; number + 1])
 }
 
+
+
+// This wont compile because you could be doing multiple inserts. so multiple borrows will occur
+// #[allow(dead_code)]
+// fn fib_memo(n: usize, memo: &mut HashMap<usize, usize>) -> usize {
+    // match memo.get(&n) {
+        // Some(val) => return *val,
+        // None => {
+            // match n {
+                // 0 | 1 | 2 => return 1,
+                // _ => {
+                    // memo.insert(n, fib_memo(n -1, memo) + fib_memo(n -2, memo));
+                    // return *memo.get(&n).unwrap();
+                // }
+            // }
+        // }
+
+    // }
+// }
+
+
+
+#[allow(dead_code)]
+fn favorite_fib(n: usize) -> usize {
+    fn fib_memoization(n: usize, memo: &mut HashMap<usize, usize>) -> usize {
+        if let Some(v) = memo.get(&n) {
+            return *v;
+        }
+
+        let v = match n {
+            0 | 1 | 2 => 1,
+            _ => fib_memoization(n-2, memo) + fib_memoization(n-1, memo),
+        };
+
+        memo.insert(n, v);
+        v
+    }
+
+    let mut memo = HashMap::new();
+    fib_memoization(n, &mut memo)
+}
+
 #[cfg(test)]
 pub mod test {
-    use super::{fib_recursive, fib};
+    use super::{fib_recursive, fib, favorite_fib};
 
     #[test]
     fn fib_recursive_base_case() {
@@ -40,6 +84,18 @@ pub mod test {
         assert_eq!(fib_recursive(5), 5);
         // This case will take a long time with a pure recursive solution
         // assert_eq!(fib_recursive(50), 12586269025);
+    }
+
+    #[test]
+    fn fib_memoization_base_case() {
+        assert_eq!(favorite_fib(1), 1);
+        assert_eq!(favorite_fib(2), 1);
+    }
+
+    #[test]
+    fn fib_memoizatoin_non_base_case() {
+        assert_eq!(favorite_fib(5), 5);
+        assert_eq!(favorite_fib(50), 12586269025);
     }
 
     #[test]
