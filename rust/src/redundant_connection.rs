af4364fa76a6e92da62b63d0da06c7b3de68f131
ragnyll <ragnyll@gallowzhumour.dev>
2021-12-18 18:46:17 -0600

    bad solution redundant connection

--- rust/src/redundant_connection.rs
+++ rust/src/redundant_connection.rs
@@ -3,6 +3,7 @@
 // You are given a graph that started as a tree with n nodes labeled from 1 to n, with one additional edge added. The added edge has two different vertices chosen from 1 to n, and was not an edge that already existed. The graph is represented as an array edges of length n where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the graph.
 //
 // Return an edge that can be removed so that the resulting graph is a tree of n nodes. If there are multiple answers, return the answer that occurs last in the input.
+// TODO: THIS IS WRONG!
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::collections::VecDeque;
@@ -46,7 +47,7 @@ fn bf_has_path(adjacency_list: &HashMap<i32, HashSet<i32>>, src: i32, dest: i32)
     false
 }
 
-fn build_adjacency_list(edges: Vec<Vec<i32>>) -> HashMap<i32, HashSet<i32>> {
+fn build_adjacency_list(edges: &Vec<Vec<i32>>) -> HashMap<i32, HashSet<i32>> {
     // adj list will be a hash map mapping a course to the prerequisite
     let mut adj_list: HashMap<i32, HashSet<i32>> = HashMap::with_capacity(edges.len());
 
@@ -79,40 +80,21 @@ fn build_adjacency_list(edges: Vec<Vec<i32>>) -> HashMap<i32, HashSet<i32>> {
 }
 
 fn find_redundant_connection(edges: Vec<Vec<i32>>) -> Vec<i32> {
-    let mut graph: HashMap<i32, HashSet<i32>> = HashMap::new();
+    let adj_list = build_adjacency_list(&edges);
+    let mut visited = HashSet::with_capacity(edges.len());
+    let mut edge_to_remove = vec![];
+
     for edge in edges {
-        if graph.contains_key(&edge[0]) && graph.contains_key(&edge[1]) && df_has_path(&graph, edge[0], edge[1]) {
-            return vec![edge[0], edge[1]]
+        if visited.contains(&edge[0]) && visited.contains(&edge[1]) {
+            edge_to_remove = edge.clone();
         }
-
-        match graph.get(&edge[0]) {
-            Some(connected_nodes) => {
-                let mut new_node_set = connected_nodes.clone();
-                new_node_set.insert(edge[1]);
-                graph.insert(edge[0], new_node_set);
-            }
-            None => {
-                // the rust 2021 syntax of hashset from is just better
-                let mut new_hs = HashSet::new();
-                new_hs.insert(edge[1]);
-                graph.insert(edge[0], HashSet::from([edge[1]]));
-            }
-        };
-
-        match graph.get(&edge[1]) {
-            Some(connected_nodes) => {
-                let mut new_node_set = connected_nodes.clone();
-                new_node_set.insert(edge[0]);
-                graph.insert(edge[1], new_node_set);
-            }
-            None => {
-                graph.insert(edge[1], HashSet::from([edge[0]]));
-            }
-        };
-
+        if bf_has_path(&adj_list, edge[0], edge[1]) {
+            visited.insert(edge[0]);
+            visited.insert(edge[1]);
+        }
     }
 
-    vec![]
+    edge_to_remove
 }
 
 #[cfg(test)]
@@ -128,7 +110,7 @@ mod test {
         expected.insert(2, HashSet::from([1, 3]));
         expected.insert(3, HashSet::from([2]));
 
-        assert_eq!(build_adjacency_list(edges), expected);
+        assert_eq!(build_adjacency_list(&edges), expected);
     }
 
 }
